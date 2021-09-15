834bf9b36a3f556b534c2878be625264a5aae625
Daniel Liu <63995385+DanielZFLiu@users.noreply.github.com>
2021-09-15 00:14:33 -0400

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -6,14 +6,14 @@ import firestore from 'firebase/firestore'
 
 //Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDoT-agzxm84y-5xfUtLapXmNcHdzJt7cU",
+    apiKey: ...,
     authDomain: "blueprint-club-forum.firebaseapp.com",
     databaseURL: "https://blueprint-club-forum.firebaseio.com",
     projectId: "blueprint-club-forum",
     storageBucket: "blueprint-club-forum.appspot.com",
-    messagingSenderId: "648208137715",
-    appId: "1:648208137715:web:32855bec64fb8373346686",
-    measurementId: "G-C817MJBTER"
+    messagingSenderId: ...,
+    appId: ...,
+    measurementId: ...
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
